bd8d79d27a8348d9f854543bbe85d9b93d0c5b4b
Bogdan <31548234+OnshaBogdan@users.noreply.github.com>
2019-06-13 12:21:55 +0300

    Minimal working api for upload/get picture from firebase storage

--- application.py
+++ application.py
@@ -1,18 +1,22 @@
-import json
+from flask import Flask, request, jsonify, send_file
 
-from flask import Flask, request, jsonify
-
+import firebase_driver
 
 app = application = Flask(__name__)
 
-@app.route('/upload', methods=['POST'])
-def uplaoad():
-    data = json.loads(request.data)
-    file = request.files['picture']
-    print(data)
-    print(file)
 
-    return jsonify(True)
+@app.route('/upload/<string:source>', methods=['POST'])
+def upload(source):
+    file = request.files['picture']
+    firebase_driver.upload(file, source)
+    return jsonify(''), 200
+
+
+@app.route('/<string:source>', methods=['GET'])
+def get(source):
+    file_path = firebase_driver.get(source)
+    return send_file(file_path), 200
+
 
 if __name__ == '__main__':
-    app.run(port=8000, debug= True)
+    app.run(port=8000, debug=True)
--- firebase_driver.py
+++ firebase_driver.py
@@ -6,6 +6,13 @@ firebase = pyrebase.initialize_app(config)
 
 db = firebase.database()
 storage = firebase.storage()
+temp_file_path = '.temp/file.jpg'
 
 
-storage.child("images").put("example.jpg")
+def get(source):
+    storage.child("images").child(source).download(temp_file_path)
+    return temp_file_path
+
+
+def upload(file, source):
+    storage.child('images').child(source).put(file)
